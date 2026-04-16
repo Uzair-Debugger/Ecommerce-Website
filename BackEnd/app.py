5f08d33fd5b5b13a7cb4c7aa6088dfe350815532
Uzair-Debugger <smuzair14cse@gmail.com>
2026-04-16 15:56:16 +0500

    Added app instance to app.py

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -70,3 +70,6 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+# Vercel requires a direct app instance at module level
+app = create_app()
